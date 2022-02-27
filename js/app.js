b0d176ec60c25a6663bf4f8ecffe5d516370ef63
Mazharul <miforbd@gmail.com>
2022-02-27 22:27:04 +0600

    button click and previous result clear

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const loadData=()=>{
+    document.getElementById('display-player').innerHTML=''
     const searchField=document.getElementById('search-field')
     const searchFieldvalue=searchField.value 
     const url=`https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchFieldvalue}`
@@ -23,6 +24,7 @@ const displayData=(players)=>{
     });
 }
 const detailsShow=(id)=>{
+    document.getElementById('details-player').innerHTML=''
     const detailsPlayer=document.getElementById('details-player');
     const url=`https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${id}`
     fetch(url)
@@ -34,14 +36,17 @@ const playerDetails=(playerInfo)=>{
     const div=document.createElement('div')
     div.classList.add('player-details')
     div.innerHTML=`
+    <img id="bannerImg" src="${playerInfo.strBanner}" alt="">
     <img src="${playerInfo.strThumb}" alt="">
-    <img src="" alt="">
     <h2>${playerInfo.strPlayer}</h2>
     <h5>Country: ${playerInfo.strNationality}</h5>
+    <p>Position: ${playerInfo.strPosition}</p>
+    <p>Side: ${playerInfo.strSide}</p>
     <p>Date of birth: ${playerInfo.dateBorn}</p>
     <p>Gender: ${playerInfo.strGender}</p>
     <p>Height: ${playerInfo.strHeight}</p>
     <p>Weight: ${playerInfo.strWeight}</p>
+    
     `
     detailsPlayers.appendChild(div)
 }
